9e2958f4f2c1fbed79f5bbfb5497e3f026bfe438
CosmoBlack <soulker_mozyr@yahoo.com>
2023-04-18 20:26:45 +0200

    Initial commit

---
+++ src/test/java/pro/sky/homework215/Homework215ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework215;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework215ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
